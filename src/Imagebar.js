3fe9c1b3ef76345edcaf4fb8bf3bc672cfdc0546
Hwanghi <heeda512@naver.com>
2021-04-25 18:12:44 +0900

    Success import minimaps !

---
+++ src/Imagebar.js
@@ -0,0 +1,78 @@
+import React from "react";
+import { Stage, Layer, Image, Rect } from "react-konva";
+import useImage from "use-image";
+
+let fileList = ["room1", "room2"];
+const URLImage = ({ image }) => {
+  const [img] = useImage(image.src);
+  return (
+    <Image
+      image={img}
+      x={image.x}
+      y={image.y}
+      draggable
+      // I will use offset to set origin to the center of the image
+      offsetX={img ? img.width / 2 : 0}
+      offsetY={img ? img.height / 2 : 0}
+    />
+  );
+};
+
+const Imagebar = () => {
+  const dragUrl = React.useRef();
+  const stageRef = React.useRef();
+  const [images, setImages] = React.useState([]);
+  return (
+    <>
+      <div className="row">
+        {fileList.map((file, i) => {
+          return (
+            <img
+              key={i}
+              alt={file}
+              src={"/images/" + file + ".jpg"}
+              width={100}
+              height={100}
+              draggable="true"
+              onDragStart={(e) => {
+                dragUrl.current = e.target.src;
+              }}
+            />
+          );
+        })}
+      </div>
+      <div
+        onDrop={(e) => {
+          e.preventDefault();
+          // register event position
+          stageRef.current.setPointersPositions(e);
+          // add image
+          setImages(
+            images.concat([
+              {
+                ...stageRef.current.getPointerPosition(),
+                src: dragUrl.current,
+              },
+            ])
+          );
+        }}
+        onDragOver={(e) => e.preventDefault()}
+      >
+        <Stage
+          width={window.innerWidth}
+          height={window.innerHeight}
+          style={{ border: "1px solid grey" }}
+          ref={stageRef}
+        >
+          <Layer>
+            {images.map((image) => {
+              return <URLImage image={image} />;
+            })}
+          </Layer>
+        </Stage>
+      </div>
+    </>
+  );
+};
+
+export default Imagebar;
--- src/Room.js
+++ src/Room.js
@@ -1,45 +1,13 @@
 import React from "react";
 import { Stage, Layer, Image, Rect } from "react-konva";
 import useImage from "use-image";
+import Imagebar from "Imagebar";
 
-const URLImage = ({ image }) => {
-  const [img] = useImage(image.src);
-  return (
-    <Image
-      image={img}
-      x={image.x}
-      y={image.y}
-      // I will use offset to set origin to the center of the image
-      offsetX={img ? img.width / 2 : 0}
-      offsetY={img ? img.height / 2 : 0}
-    />
-  );
-};
 
-const Room = (props) => {
-  const dragUrl = React.useRef();
-  const stageRef = React.useRef();
-  const [images, setImages] = React.useState([]);
+const Room = () => {
   return (
     <>
-      <img
-        alt="room"
-        src="/images/oneroom.jpg"
-        draggable="true"
-        onDragStart={(e) => {
-          dragUrl.current = e.target.src;
-        }}
-      />
-      <Stage
-        width={window.innerWidth}
-        height={window.innerHeight}
-      >
-        <Layer>
-        {images.map((image) => {
-              return <URLImage image={image} />;
-        })}
-        </Layer>
-      </Stage>
+      <Imagebar />
     </>
   );
 };
