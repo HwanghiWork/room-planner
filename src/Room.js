6c51145a6cfe83bdb800a828a4d8be42b6f09c14
Hwanghi <heeda512@naver.com>
2021-05-08 04:28:01 +0900

    Refresh doesn't erase images on canvas.

--- src/Room.js
+++ src/Room.js
@@ -1,10 +1,9 @@
 import React, { useState, useRef } from "react";
 import Imagebar from "Imagebar";
-import { Stage, Layer, Image, Rect } from "react-konva";
+import { Stage, Layer, Image } from "react-konva";
 import useImage from "use-image";
-import ImageUploading from "react-images-uploading";
 
-const URLImage = ({ image }) => {
+const URLImage = ({ image, index }) => {
   const [img] = useImage(image.src);
   return (
     <Image
@@ -15,7 +14,14 @@ const URLImage = ({ image }) => {
       // I will use offset to set origin to the center of the image
       offsetX={img ? img.width / 2 : 0}
       offsetY={img ? img.height / 2 : 0}
-      onDragEnd={(e) => {}}
+      onDragEnd={(e) => {
+        const { attrs:{x, y} } = e.target;
+        let canvasImages = JSON.parse(localStorage.getItem("canvasImages"));
+        // Search this image and change values
+        canvasImages[index].x = x;
+        canvasImages[index].y = y;
+        localStorage.setItem("canvasImages", JSON.stringify(canvasImages));
+      }}
     />
   );
 };
@@ -23,36 +29,17 @@ const URLImage = ({ image }) => {
 const Room = () => {
   const dragUrl = useRef();
   const stageRef = useRef();
-  const [images, setImages] = useState([]);
-  const [ruler, setRuler] = useState({
-    w: 0,
-    h: 0,
-    x: 0,
-    y: 0,
-  });
-  const onChange = (event) => {
-    const {
-      target: { name, value },
-    } = event;
-    let tmpRuler = ruler;
-    const num = parseInt(value);
-    switch (name) {
-      case "width": tmpRuler.w = num; break;
-      case "height": tmpRuler.h = num; break;
-      case "offsetX": tmpRuler.x = num; break;
-      case "offsetY": tmpRuler.y = num; break;
-    }
-    setRuler(tmpRuler);
-    console.log(ruler)
-  };
+  const [canvasImages, setCanvasImages] = useState(
+    JSON.parse(localStorage.getItem("canvasImages")) || []
+  );
+  
+  React.useEffect(() => {
+    localStorage.setItem("canvasImages", JSON.stringify(canvasImages));
+    console.log(JSON.parse(localStorage.getItem("canvasImages")))
+  }, [canvasImages]);
+
   return (
     <>
-      {/* <div className="d-flex">
-        <input name="width" type="number" onChange={onChange}/>
-        <input name="height" type="number" onChange={onChange}/>
-        <input name="offsetX" type="number" onChange={onChange}/>
-        <input name="offsetY" type="number" onChange={onChange}/>
-      </div> */}
       <Imagebar dragUrl={dragUrl} />
       <div
         onDrop={(e) => {
@@ -60,8 +47,8 @@ const Room = () => {
           // register event position
           stageRef.current.setPointersPositions(e);
           // add image
-          setImages(
-            images.concat([
+          setCanvasImages(
+            canvasImages.concat([
               {
                 ...stageRef.current.getPointerPosition(),
                 src: dragUrl.current,
@@ -78,17 +65,9 @@ const Room = () => {
           ref={stageRef}
         >
           <Layer>
-            {images.map((image, i) => {
-              return <URLImage key={i} image={image} />;
+            {canvasImages.map((image, i) => {
+              return <URLImage key={i} image={image} index={i}/>;
             })}
-            <Rect
-              x={ruler.x}
-              y={ruler.y}
-              width={ruler.w}
-              height={ruler.h}
-              stroke="black"
-              style={{ border: "3px solid" }}
-            />
           </Layer>
         </Stage>
       </div>
